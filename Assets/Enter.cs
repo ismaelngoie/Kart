8d0ca25e2c95ac307823e7d51553af0627f1dea4
ismaelngoie <ismaelngoie@icloud.com>
2019-12-10 18:16:39 -0700

    Trigger on Start
    On start it triggers

---
+++ Assets/Enter.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enter : MonoBehaviour
+{
+   public GameObject movePlatform;
+
+   private void OnTriggerEnter()
+   {
+   		movePlatform.SetActive(true);
+   }
+}
